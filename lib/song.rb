937a383fd6d5643bd2b720be2f91da2125ee8d51
Anthony Leon <acidknights89@gmail.com>
2020-09-24 00:46:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,7 +50,7 @@ class Song
   def self.create_from_filename(name)
     rows = name.split("/n")
     people = rows.collect do |row|
-      data = row.split(" - ")
+      data = row.split(" - ")[1].chomp(".mp3")
   end
 
 end
